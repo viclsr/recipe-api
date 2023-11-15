dd7f7990f280ed68e8ddb3da8e807465a0c259a3
Victoria Ribeiro <victorialsr0112@gmail.com>
2023-11-15 13:41:41 -0300

    feat: change recipeName to name

--- src/routes.js
+++ src/routes.js
@@ -1,37 +1,37 @@
-import { Router } from 'express';
+import { Router } from "express";
 
-import { check, validationResult } from 'express-validator';
+import { check, validationResult } from "express-validator";
 
 let recipes = [];
 
 const validateRecipePostRequest = [
-  check('id').isInt(),
-  check('recipeName').isString(),
-  check('prepTime').isString(),
-  check('ingredients').isArray(),
-  check('instructions').isString(),
-  check('category').isString(),
-  check('image').optional().isString(),
+  check("id").isInt(),
+  check("name").isString(),
+  check("prepTime").isString(),
+  check("ingredients").isArray(),
+  check("instructions").isString(),
+  check("category").isString(),
+  check("image").optional().isString(),
 ];
 
 const routes = new Router();
 
-routes.get('/recipes', (req, res) => {
+routes.get("/recipes", (req, res) => {
   res.json(recipes);
 });
 
-routes.get('/recipes/:id', (req, res) => {
+routes.get("/recipes/:id", (req, res) => {
   const id = parseInt(req.params.id);
-  const recipe = recipes.find(r => r.id === id);
+  const recipe = recipes.find((r) => r.id === id);
 
   if (recipe) {
     res.json(recipe);
   } else {
-    res.status(404).send('Receita não encontrada');
+    res.status(404).send("Receita não encontrada");
   }
 });
 
-routes.post('/recipes', validateRecipePostRequest, (req, res) => {
+routes.post("/recipes", validateRecipePostRequest, (req, res) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
@@ -39,25 +39,25 @@ routes.post('/recipes', validateRecipePostRequest, (req, res) => {
   }
 
   const allowedFields = [
-    'id',
-    'recipeName',
-    'prepTime',
-    'ingredients',
-    'instructions',
-    'category',
-    'image',
+    "id",
+    "name",
+    "prepTime",
+    "ingredients",
+    "instructions",
+    "category",
+    "image",
   ];
 
   const sentFields = Object.keys(req.body);
   const invalidFields = sentFields.filter(
-    field => !allowedFields.includes(field)
+    (field) => !allowedFields.includes(field)
   );
 
   if (invalidFields.length > 0) {
     return res.status(400).json({
       errors: [
         {
-          msg: 'Campos inválidos enviados no corpo da solicitação.',
+          msg: "Campos inválidos enviados no corpo da solicitação.",
           invalidFields,
         },
       ],
@@ -70,22 +70,22 @@ routes.post('/recipes', validateRecipePostRequest, (req, res) => {
   res.status(201).json(newRecipe);
 });
 
-routes.put('/recipes/:id', (req, res) => {
+routes.put("/recipes/:id", (req, res) => {
   const id = parseInt(req.params.id);
-  const recipeIndex = recipes.findIndex(r => r.id === id);
+  const recipeIndex = recipes.findIndex((r) => r.id === id);
 
   if (recipeIndex !== -1) {
     recipes[recipeIndex] = { ...recipes[recipeIndex], ...req.body };
     res.json(recipes[recipeIndex]);
   } else {
-    res.status(404).send('Receita não encontrada');
+    res.status(404).send("Receita não encontrada");
   }
 });
 
-routes.delete('/recipes/:id', (req, res) => {
+routes.delete("/recipes/:id", (req, res) => {
   const id = parseInt(req.params.id);
-  recipes = recipes.filter(r => r.id !== id);
-  res.send('Receita excluída com sucesso');
+  recipes = recipes.filter((r) => r.id !== id);
+  res.send("Receita excluída com sucesso");
 });
 
 export default routes;
