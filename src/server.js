7b4a5151f9d3ae4d896b1e82d6e1e788bee20d66
Victoria Ribeiro <victorialsr0112@gmail.com>
2023-11-21 23:28:28 -0300

    feat: increase payload limit

--- src/server.js
+++ src/server.js
@@ -1,18 +1,19 @@
-import express from 'express';
-import bodyParser from 'body-parser';
+import express from "express";
+import bodyParser from "body-parser";
 
-import routes from './routes.js';
+import routes from "./routes.js";
 
-import cors from 'cors';
+import cors from "cors";
 
 const app = express();
 app.use(cors());
 
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: "30mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "30mb" }));
 
 const PORT = process.env.PORT || 5555;
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   return res.json("Servidor funcionando com sucesso!");
 });
 
